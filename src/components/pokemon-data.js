818975989c2f9eca16f38fcf27ae8202b6978c1d
PandesalPanpan <petermarticio@gmail.com>
2025-10-21 11:49:52 +0800

    Feat: 10 Pokemons

--- src/components/pokemon-data.js
+++ src/components/pokemon-data.js
@@ -7,7 +7,40 @@ const pokemonList = [
     {
         name: 'Pikachu',
         imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/25.png'
-    }
+    },
+    {
+        name: 'Clefairy',
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/35.png'
+    },
+    {
+        name: 'Bellsprout',
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/69.png'
+    },
+    {
+        name: 'Hippopotas',
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/449.png'
+    },
+    {
+        name: 'Phanpy',
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/231.png'
+    },
+    {
+        name: 'Rowlet',
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/722.png'
+    },
+    {
+        name: 'Noibat',
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/714.png'
+    },
+    {
+        name: 'Machop',
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/66.png'
+    },
+    {
+        name: 'Vaporeon',
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/134.png'
+    },
+
 ]
 
 export default pokemonList;
